937c4db27b162abd63be16228194c6fb02d36960
MinGW <mirinae@skuniv.ac.kr>
2021-05-28 01:57:26 +0900

    Update

--- src/main/java/com/libraryManage/LibraryManageApplication.java
+++ src/main/java/com/libraryManage/LibraryManageApplication.java
@@ -2,19 +2,11 @@ package com.libraryManage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import com.libraryManage.Config.JavaConfig;
-import com.libraryManage.Controller.*;
 
 @SpringBootApplication
 public class LibraryManageApplication {
 
 	public static void main(String[] args) {
-//		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(JavaConfig.class);
-//
-//		MainController controllers = (MainController)ctx.getBean("mainController");
-//		
 		SpringApplication.run(LibraryManageApplication.class, args);
 	}
 
