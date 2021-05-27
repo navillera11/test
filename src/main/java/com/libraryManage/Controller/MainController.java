79229d1780d3a42f9eb4c584bf3d21d462deff92
MinGW <mirinae@skuniv.ac.kr>
2021-05-28 02:33:14 +0900

    Update Controller

--- src/main/java/com/libraryManage/Controller/MainController.java
+++ src/main/java/com/libraryManage/Controller/MainController.java
@@ -1,5 +1,8 @@
 package com.libraryManage.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -7,10 +10,22 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class MainController {
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index(Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+
+		String userId = (String) session.getAttribute(session.getId());
+
+		if (userId != null) {
+			if(userId.equals("admin"))
+				return "admin_index";
+			else
+				return "user_index";
+		}
+
 		return "index";
 	}
 	
+	// 관리자 페이지 테스트용
 	@GetMapping("/admin_index")
 	public String admin_index() {
 		return "admin_index";
