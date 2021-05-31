4c148d193aed6c51fd7afab7497e09e97a7aae5e
jee00609 <integral76@daum.net>
2021-05-31 23:41:27 +0900

    게시글 공개 설정 여부 페이지 추가

--- src/main/java/com/libraryManage/Controller/AdminBoardController.java
+++ src/main/java/com/libraryManage/Controller/AdminBoardController.java
@@ -1,8 +1,17 @@
 package com.libraryManage.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import com.libraryManage.DAO.BoardDAO;
+import com.libraryManage.DAO.BookDAO;
+import com.libraryManage.DAO.GoodDAO;
+import com.libraryManage.DTO.BoardDTO;
+import com.libraryManage.Service.BoardService;
 
 @Controller
 @RequestMapping(value = "/admin/alarm/board/*")
@@ -10,6 +19,26 @@ public class AdminBoardController {
 	// 관리자 페이지 중
 	// 알림 부분 중
 	// 자유게시판 부분
+	
+	@Autowired
+	BoardService boardService;
+	@Autowired
+	BoardDAO boardDAO;
+	/*
+	 * @Autowired GoodDAO goodDAO;
+	 * 
+	 * @Autowired BookDAO bookDAO;
+	 */
+	
+	//게시판 비공개
+	@RequestMapping(value = "/board_update", method = RequestMethod.GET)
+	public String admin_alarm_good_delete(Model model) {
+		List<BoardDTO> boardList = boardDAO.showAll();
+
+		model.addAttribute("boardList", boardList);
+
+		return "admin_alarm_board_update";
+	}
 
 	// @Autowired
 //	BoardService boardService;
---
+++ src/main/java/com/libraryManage/DAO/BoardDAO.java
@@ -0,0 +1,44 @@
+package com.libraryManage.DAO;
+
+import org.springframework.jdbc.core.*;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+import javax.sql.*;
+
+import com.libraryManage.DTO.*;
+
+@Component
+public class BoardDAO {
+	private BoardDTO boardDTO;
+	private JdbcTemplate jdbcTemplate;
+	
+	public BoardDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public BoardDTO selectByFBID(String inputFBID) {
+		try {
+			return jdbcTemplate.queryForObject("SELECT * FROM BOARD WHERE FBID=?;",
+					(rs, rowNum) -> new BoardDTO(rs.getInt("FBID"), rs.getString("EMAIL"), rs.getString("TITLE"),
+							rs.getString("CONTENT"), rs.getDate("DATE"), rs.getString("PUBLIC")),
+					inputFBID);
+		} catch (Exception ex) {
+			return null;
+		}
+	}
+	
+	public List<BoardDTO> showAll() {
+		List<BoardDTO> result = jdbcTemplate.query("SELECT * FROM BOARD;", (rs, rowNum) -> {
+			BoardDTO boardDTO = new BoardDTO(rs.getInt("FBID"), rs.getString("EMAIL"), rs.getString("TITLE"),
+					rs.getString("CONTENT"), rs.getDate("DATE"), rs.getString("PUBLIC"));
+			return boardDTO;
+		});
+		return result;
+	}
+	
+	public void updatePublic(BoardDTO boardDTO, String newPublic) {
+		jdbcTemplate.update("UPDATE BOARD SET PUBLIC='" + newPublic + "' WHERE FBID=" + boardDTO.getBoardID() + ";");
+	}
+}
