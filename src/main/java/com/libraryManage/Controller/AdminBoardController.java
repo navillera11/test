084d3f2d5ca3d91e515ebc4723eabf5c56f969ed
MinGW <mirinae@skuniv.ac.kr>
2021-05-31 22:07:17 +0900

    게시판 컨트롤러 생성

--- src/main/java/com/libraryManage/Controller/AdminBoardController.java
+++ src/main/java/com/libraryManage/Controller/AdminBoardController.java
@@ -1,7 +1,23 @@
 package com.libraryManage.Controller;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/admin/alarm/board/*")
 public class AdminBoardController {
 	// 관리자 페이지 중
 	// 알림 부분 중
 	// 자유게시판 부분
+
+	// @Autowired
+//	BoardService boardService;
+	// @Autowired
+	// BoardDAO boardDAO;
+
+	// 자유게시판 목록 페이지 이동
+
+	// 자유게시판 세부 페이지 이동
+
 }
